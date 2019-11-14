591d9f7b3f470fac003841e53a7414deaf9aad50
Joseph Cerankowski <Joecerankowski@gmail.com>
2019-11-13 19:46:05 -0500

    Create Numeral_System_Converter_Step_1.java

---
+++ Numeral_System_Converter_Step_1.java
@@ -0,0 +1,12 @@
+package converter;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Scanner scanner = new Scanner(System.in);
+        int input = 10;
+        int binary = 0b1010;
+        System.out.println(input + " = 0b" + Integer.toBinaryString(binary));
+    }
+}
